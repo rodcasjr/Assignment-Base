6f3aba6a36ca2a91f806a49bce2fc290a4d763a5
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-02-28 15:54:35 -0500

    commit update to json package and js server files

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
+import reload from 'livereload';
+import connectReload from 'connect-livereload';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const port = process.env.PORT || 3000;
+const staticFolder = 'public';
 
+// Add some auto-reloading to our server
+const liveReloadServer = reload.createServer();
+liveReloadServer.watch(path.join(__dirname, staticFolder));
+
+// Configure express
+app.use(connectReload());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(staticFolder));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,22 +31,27 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.route('/api')
-  .get((req, res) => {
+  .get(async (req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
-  })
-  .post(async (req, res) => {
-    console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
     console.log('data from fetch', json);
     res.json(json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    //console.log('Now send something back to your client');
+    res.send('hello world');
   });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}!`);
+});
+
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
 });
